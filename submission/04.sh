a79fae5b47bed216b5e2a8dbc8af5236927f31b5
BalogunMalikO <balogunmalik4@gmail.comgit>
2025-04-16 00:58:55 +0100

    week 5

--- submission/04.sh
+++ submission/04.sh
@@ -1,2 +1,20 @@
 # Create a CLTV script with a timestamp of 1495584032 and public key below:
-# publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+
+#CONVERT TO HEX
+hex=$(printf '%08x\n' 1495584032 | sed 's/^\(00\)*//');
+
+
+#CONVERT TO LITTLE ENDIAN
+lehex=$(echo $hex | tac -rs .. | echo "$(tr -d '\n')");
+
+#CRETEA CLTV
+
+PUBKEY_HASH=$(echo $publicKey | xxd -r -p | openssl sha256 -binary | openssl rmd160 | cut -d' ' -f2)
+
+SIZE=$(echo -n $lehex | wc -c | awk '(print $1/2)')
+
+#OP_PUSHDATA 4bytes(SIZE) <TIMESTAMP> OP_CHECKLOCKTIMEVERIFY OP_DROP OP_DUP OP_HASH160 OP_PUSHDATA 20bytes <PUBKEY_HASH> OP_EQUALVERIFY OP_CHECKSIG 
+serializedScript="0$SIZE"$lehex"b17576a914"$pubkeyhash"88ac" 
+
+echo $serializedScript
--- submission/05.sh
+++ submission/05.sh
@@ -1,2 +1,20 @@
 # Create a CSV script that would lock funds until one hundred and fifty blocks had passed
-# publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+
+BLOCKHEIGHTS=150
+
+hex=$(printf '%08x\n' $BLOCKHEIGHTS | sed 's/^\(00\)*//');
+
+hexfirst=$(echo $hex | cut -c1)
+[[ 0x$hexfirst -gt 0x7 ]] && hex="00"$hex
+
+lehex=$(echo $hex | tac -rs .. | echo "$(tr -d '\n')");
+
+SIZE=$(echo -n $lehex | wc -c | awk '(print $1/2)')
+
+PUBKEY_HASH=$(echo $publicKey | xxd -r -p | openssl sha256 -binary | openssl rmd160 | cut -d' ' -f2)
+
+#OP_PUSHDATA <BLOCKHEIGHT_LE_LENGTH> <BLOCKHEIGHT> OP_CHECKSEQUENCEVERIFY OP_DROP OP_DUP OP_HASH160 OP_PUSHDATA <PUBKEYHASHLENGTH> OP_EQUALVERIFY OP_CHECKSIG 
+CSV_SCIPT="0$SIZE"$LEHEX"b27576a914"$PUBKEY_HASH"88ac" 
+
+echo $CSV_SCIPT
