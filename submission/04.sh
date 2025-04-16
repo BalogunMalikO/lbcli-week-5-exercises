9cb7afc80505edcbd3b7fed0d52df57952f111ec
BalogunMalikO <balogunmalik4@gmail.comgit>
2025-04-16 01:10:37 +0100

    week 5

--- submission/04.sh
+++ submission/04.sh
@@ -4,7 +4,6 @@ publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 #CONVERT TO HEX
 hex=$(printf '%08x\n' 1495584032 | sed 's/^\(00\)*//');
 
-
 #CONVERT TO LITTLE ENDIAN
 lehex=$(echo $hex | tac -rs .. | echo "$(tr -d '\n')");
 
@@ -15,6 +14,6 @@ PUBKEY_HASH=$(echo $publicKey | xxd -r -p | openssl sha256 -binary | openssl rmd
 SIZE=$(echo -n $lehex | wc -c | awk '{print $1/2}')
 
 #OP_PUSHDATA 4bytes(SIZE) <TIMESTAMP> OP_CHECKLOCKTIMEVERIFY OP_DROP OP_DUP OP_HASH160 OP_PUSHDATA 20bytes <PUBKEY_HASH> OP_EQUALVERIFY OP_CHECKSIG 
-serializedScript="0$SIZE"$lehex"b17576a914"$pubkeyhash"88ac" 
+serializedScript="0$SIZE"$lehex"b17576a914"$PUBKEY_HASH"88ac" 
 
 echo $serializedScript
